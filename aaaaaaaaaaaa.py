a2168599ed988c414fe7a24fafba9312ba52b77a
Gustavo Patricio Novais <162983653+PebblesII@users.noreply.github.com>
2024-04-24 08:14:26 -0300

    Add files via upload

---
+++ aaaaaaaaaaaa.py
@@ -0,0 +1,28 @@
+ListaNomes = []
+
+ListaNomes.append("AAAAAAAAAAAAA")
+ListaNomes.append("AAAAAAAAAAAAAAAA")
+
+print(ListaNomes)
+print(ListaNomes[0])
+
+for nome in ListaNomes:
+    print(nome)
+
+
+#dicionario
+
+primeiroDic = {}
+primeiroDic["nome"] = "G"
+primeiroDic["letra"] = "A"
+primeiroDic["idade"] = 90000000000000000
+
+print(primeiroDic)
+print("\n" * 30)
+print(primeiroDic["idade"])
+
+
+#anatomia
+
+#lista[indice] = Valor
+#dicionario[Chave] = Valor
