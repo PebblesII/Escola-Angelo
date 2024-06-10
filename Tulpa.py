45cb3ea0ffd38884fddef0d5a402b19c5475eef7
PebblesII <shobell70@gmai.com>
2024-06-10 08:10:40 -0300

    Tulpa

---
+++ Tulpa.py
@@ -0,0 +1,22 @@
+Lista = ["Escola", 2, True, 0.2]
+Dic = {"Escola" : "Almeida", "Idade" : 2}
+
+print(Lista)
+print(Dic)
+
+
+Tulpa = ("Bolas", 2)
+print(Tulpa)
+
+Tulpa2 = "Rua", 2, False
+print(Tulpa2)
+
+print(Tulpa2[-1])
+
+Lista2 = list(Tulpa2)
+print(Lista2)
+
+Lista2[0] = "Avenida"
+
+Tulpa3 = tuple(Tulpa)
+print(Tulpa3)
