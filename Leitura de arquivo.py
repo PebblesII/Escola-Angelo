adf994c75fff57f6d7e987f3e9cf1561c9048eaf
PebblesII <shobell70@gmai.com>
2024-06-17 08:17:45 -0300

    Arquivo

---
+++ Leitura de arquivo.py
@@ -0,0 +1,23 @@
+with open("Arquivo.txt", "w") as arquivo:
+       arquivo.write("Ola mundo!\n")
+       arquivo.write("Estou aqui!\n")
+       linhas = ["Proxima linha", "Outra linha"]
+       arquivo.writelines(linhas)
+
+#with open("Arquivo.txt", "r") as arquivo:
+#        arquivo = arquivo.read()
+#        linhas = arquivo.readlines()
+#        for linhas in arquivo:
+#                print(linhas)
+
+
+texto = "Mesagem"
+with open("Arquivo.txt", "w") as arquivo:
+       arquivo.write(texto)
+
+arquivo = open("test2.txt", "w")
+
+arquivo.write(texto)
+arquivo.write("\nOutraLinha\n")
+arquivo.write("fim do arquivo")
+arquivo.close()
